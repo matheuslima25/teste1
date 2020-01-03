a58d1f2d5d279d7ee100053558a3891a0c94cdaa
Matheus Lima <josematheuslima25@gmail.com>
2020-01-03 15:17:31 -0300

    Comentários

--- palindromo.py
+++ palindromo.py
@@ -1,15 +1,15 @@
 import re
 
 
-p = 'luz azul'
+p = 'luz azul'  # Nome exemplo
 
 
 def palindromo(nome):
 
-    nome = re.sub(r"\s+", "", nome) #Retirando os espaços do nome dado
-    l = len(nome) #Tamanho da String
-    s1 = nome[l::-1]  #String invertida
-    s2 = re.sub(r"\s+", "", s1)   #String invertida sem espaço
+    nome = re.sub(r"\s+", "", nome)     # Retirando os espaços do nome dado
+    l = len(nome)   # Tamanho da String
+    s1 = nome[l::-1]  # String invertida
+    s2 = re.sub(r"\s+", "", s1)   # String invertida sem espaço
 
     if s2 == nome:
         print('O nome dado é palindromo.')
